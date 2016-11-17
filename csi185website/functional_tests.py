079a65fea8228c907f1d4173700d62349d51be98
Jacob Saxon <Jake@Jacobs-MacBook-Pro-3.local>
2016-11-16 19:21:17 -0500

    saving on Nov 16

--- csi185website/functional_tests.py
+++ csi185website/functional_tests.py
@@ -47,31 +47,36 @@ class NewVisitorTest(unittest.TestCase):
         # In general this is how we check for images on a page.
 
         m=self.browser.find_element_by_tag_name('img')
-        self.assertIn('homebrew.png',m.get_attribute('src'))
+        self.assertIn('red.jpg',m.get_attribute('src'))
 
         # We check here for the title of your home page.
         # uncomment the next lines and change the text when you set your title.
         # put your title in place of "The Title of My Home Page"
 
-        h=self.browser.find_element_by_css_selector('h1')
-        self.assertIn("Modern Love From a Distance",h.text)
+        # h=self.browser.find_element_by_css_selector('h1')
+        # self.assertIn("Modern Love From a Distance",h.text)
 
         # There is an area specified around the computer keyboard.
         # the 'id' of this area is 'keyboard'
 
         # this is how we find that area.
-        a=self.browser.find_element_by_id('keyboard')
+        a=self.browser.find_element_by_id('begin')
 
         # this is how we click on it.
         a.click()
 
+        self.assertIn('begin',self.browser.title)
+
+        h=self.browser.find_element_by_tag_name('h1')
+        m=self.browser.find_element_by_tag_name('img')
+
         # after clicking on it, we should see the next page.
-        e=self.browser.find_element_by_css_selector("h1")
-        self.assertIn('Keyboards',e.text)
+        # e=self.browser.find_element_by_css_selector("h1")
+        # self.assertIn('Keyboards',e.text)
 
         # The page should have a picture of a teletype machine. 
-        m=self.browser.find_element_by_tag_name('img')
-        self.assertIn('teletype.jpg',m.get_attribute('src'))
+        # m=self.browser.find_element_by_tag_name('img')
+        # self.assertIn('teletype.jpg',m.get_attribute('src'))
     
 
 if __name__=="__main__":
